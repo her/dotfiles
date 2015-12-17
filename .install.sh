70ed8233ffc17b08ae7af94c70b336c49b449614
melanie <her@users.noreply.github.com>
2015-12-16 16:55:24 -0800

    now supports files and hidden files

--- .install.sh
+++ .install.sh
@@ -9,8 +9,14 @@
 dir=/Users/mb/etc/dotfiles         #dotfiles directory  
 olddir=/Users/mb/etc/dotfiles_old  #dotfiles backup     
 files="                    
-         vimrc
+         git-prompt.sh
+	 git-completion.bash
 "
+
+dfiles=" 
+	 vimrc
+"
+
 
 ##########Process
 
@@ -24,12 +30,21 @@ echo "Changing to $dir"
 cd $dir
 echo "...done"
 
-#clean ~/ and create symlinks 
+#clean ~/ and create symlinks for dfiles
 
 for file in $files; do
   echo "Cleaning old symlinks mv to oldir"
-  mv ~/.$file ~/etc/dotfiles_old/
+  mv ~/$file ~/etc/dotfiles_old/
   echo "Creating symlinks from dir to home" 
-  ln -s ~/etc/dotfiles/$file ~/.$file
+  ln -s ~/etc/dotfiles/$file ~/$file
+done
+
+#clean ~/ and create symlinks for files
+
+for dfile in $dfiles; do
+  echo "Cleaning old symlinks mv to oldir"
+  mv ~/.$dfile ~/etc/dotfiles_old/
+  echo "Creating symlinks from dir to home" 
+  ln -s ~/etc/dotfiles/$dfile ~/.$dfile
 done
 
