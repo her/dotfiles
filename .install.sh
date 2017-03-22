7aafba210d4593792a354c94259b4d5a9705a192
melanie <her@users.noreply.github.com>
2017-03-22 15:19:40 -0700

    Modified variable names for clarity.

--- .install.sh
+++ .install.sh
@@ -27,19 +27,17 @@ mkdir -p $trash
 echo "Changing to $dir"
 cd $dir
 
-# Removing and replacing old files. Creating symlinks for new files. 
+# Removing and replacing old files. Creating symbolic links for new files.
+echo "Removing and replacing old files. Creating symbolic links for new files." 
 for file in $files; do
-  echo "Removing old symbolic links."
   mv ~/$file ~/etc/dotfiles_old/
-  echo "Creating symbolic links for new files." 
   ln -s ~/etc/dotfiles/$file ~/$file
 done
 
-# Removing and replacing old dotfiles. Creating symlinks for new dotfiles.
+# Removing and replacing old dotfiles. Creating symbolic links for new dotfiles.
+echo "Removing and replacing old dotfiles. Creating symbolic links for new dotfiles."
 for dotfile in $dotfiles; do
-  echo "Removing old symbolic links."
   mv ~/.$dotfile ~/etc/dotfiles_old/
-  echo "Creating symbolic links for new files." 
   ln -s ~/etc/dotfiles/$dotfile ~/.$dotfile
 done
 
