e0f5ed46267545f95c20587076561049f5afbea9
melanie <hello@berkley.io>
2017-10-11 23:43:10 -0700

    Check for Xcode CLI tools, prompt user before starting.

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Creates symbolic links for miscellaneous configuration files.
+# Create links for configuration files and install certain niceties.
 
 # Initializing variables.
 CURL='/usr/bin/curl'
@@ -14,44 +14,74 @@ dotfiles="
   gitconfig
 "
 
-# Making a temporary directory to store trash.
-echo "Making a temporary directory to store trash."
-mkdir -p $trash
+# First check if Xcode command line tools are installed
+echo "Checking for Xcode Command Line Tools..."
+if type xcode-select >&- && xpath=$( xcode-select --print-path ) &&
+  test -d "${xpath}" && test -x "${xpath}" ; then
+  echo "Ok"
+  echo
 
-# Changing directory.
-echo "Changing to $dir"
-cd $dir
+  # Prompt user before running link.sh
+  read -p "Setup environment and install Homebrew? y/N " -n 1 -r
+  echo
 
-# Copy bash profile.
-echo "Adding bash profile."
-cp bash_profile ~/.bash_profile
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
 
-# Removing and replacing old dotfiles. Creating symbolic links for new dotfiles.
-echo "Removing and replacing old dotfiles. Creating symbolic links for new dotfiles."
-for dotfile in $dotfiles; do
-  mv ~/.$dotfile ~/etc/dotfiles_old/
-  ln -s ~/etc/dotfiles/$dotfile ~/.$dotfile
-done
+    # Install Homebrew if not already installed
+    if test ! $(which brew); then
+      echo
+      echo "Installing homebrew..."
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    else
+      echo
+      echo "Homebrew already installed. Skipping..."
+    fi
 
-# Removing the trash.
-echo "Removing trash."
-cd ~/etc
-rm -rf dotfiles_old/
+    # Making a temporary directory to store trash.
+    echo
+    echo "Making a temporary directory to store trash."
+    mkdir -p $trash
 
-# Download Git Completion 
-echo "Downloading Git Completion."
-GCHTTP="https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
-$CURL $CURLARGS $GCHTTP > ~/.git-completion.bash
+    # Changing directory.
+    echo "Changing to $dir"
+    cd $dir
 
-# Download enlighten.terminal 
-echo "Downloading enlighten.terminal"
-EHTTP="https://raw.githubusercontent.com/her/enlighten/master/enlighten.terminal"
-$CURL $CURLARGS $EHTTP > ~/Downloads/enlighten.terminal
-echo "Downloaded to ~/Downloads open with terminal.app to auto import"
+    # Copy bash profile.
+    echo "Adding bash profile."
+    cp bash_profile ~/.bash_profile
 
-# Install Homebrew
-if test ! $(which brew); then
-    echo "Installing homebrew..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  else echo "Homebrew already installed."
+    # Removing and replacing old dotfiles. Creating symbolic links for new dotfiles.
+    echo "Removing and replacing old dotfiles. Creating symbolic links for new dotfiles."
+    for dotfile in $dotfiles; do
+      mv ~/.$dotfile ~/etc/dotfiles_old/
+      ln -s ~/etc/dotfiles/$dotfile ~/.$dotfile
+    done
+
+    # Removing the trash.
+    echo "Removing trash."
+    cd ~/etc
+    rm -rf dotfiles_old/
+
+    # Download Git Completion
+    echo
+    echo "Downloading Git Completion."
+    GCHTTP="https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
+    $CURL $CURLARGS $GCHTTP > ~/.git-completion.bash
+
+    # Download enlighten.terminal
+    echo
+    echo "Downloading enlighten.terminal to ~/Downloads open with terminal.app to auto import."
+    EHTTP="https://raw.githubusercontent.com/her/enlighten/master/terminal/enlighten.terminal"
+    $CURL $CURLARGS $EHTTP > ~/Downloads/enlighten.terminal
+
+    echo
+    echo "Finished."
+  else
+    echo "Aborted."
+  fi
+else
+  echo "fatal: Xcode command line tools not installed."
+  echo "Install Xcode then install tools with: xcode-select --install"
+  echo
 fi
