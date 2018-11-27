d4fa217a31e55b6f83742d1b23970a8418a04f2d
melanie <hello@berkley.io>
2018-11-27 14:59:08 -0800

    for bsd machine

---
+++ bsd_link.sh
@@ -0,0 +1,22 @@
+#!/usr/local/bin/bash
+
+# Initializing variables.
+CURL='/usr/local/bin/curl'
+CURLARGS="-f -L"
+
+cp bash_profile $HOME/.bash_profile
+cp bashrc $HOME/.bashrc
+cp curlrc $HOME/.curlrc
+cp gitconfig $HOME/.gitconfig
+cp gitignore_global $HOME/.gitignore_global
+cp psqlrc $HOME/.psqlrc
+cp vimrc $HOME/.vimrc
+
+if [ -e $HOME/.git-completion.bash ]; then
+  echo "Git Completion already exists. Skipping..."
+else
+  echo
+  echo "Downloading Git Completion."
+  GCHTTP="https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
+  $CURL $CURLARGS $GCHTTP > $HOME/.git-completion.bash
+fi
