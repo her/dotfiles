4e3bbca66447f82691943557659ce0ddd50db371
Melanie Berkley <mberkley@anduril.com>
2022-05-02 17:08:20 -0700

    add telescope for gd

--- init.lua
+++ init.lua
@@ -201,7 +201,7 @@ local lspconfig = require 'lspconfig'
 local on_attach = function(_, bufnr)
   local opts = { buffer = bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, opts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
   vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, opts)
