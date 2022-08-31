9199915980c4e28167de38df51c128150d9bc6f9
Melanie Berkley <mberkley@anduril.com>
2022-08-31 14:59:21 -0700

    config updates

--- init.lua
+++ init.lua
@@ -20,6 +20,23 @@ require('packer').startup(function()
   use 'saadparwaiz1/cmp_luasnip'
   use 'L3MON4D3/LuaSnip'
 
+  -- Diagnostics & Code Actions
+  use ({
+      'kosayoda/nvim-lightbulb',
+      requires = 'antoinemadec/FixCursorHold.nvim',
+      config = function() 
+        local bulb = require('nvim-lightbulb')
+        bulb.setup({autocmd = {enabled = true}})
+      end
+  })
+
+  -- DAP
+  use 'mfussenegger/nvim-dap'
+  use 'leoluz/nvim-dap-go'
+  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+  use 'theHamsta/nvim-dap-virtual-text'
+  use 'nvim-telescope/telescope-dap.nvim'
+
   -- Treesitter
   use 'nvim-treesitter/nvim-treesitter'
   use 'nvim-treesitter/nvim-treesitter-textobjects'
@@ -31,9 +48,12 @@ require('packer').startup(function()
   use 'kyazdani42/nvim-web-devicons'
   use 'kyazdani42/nvim-tree.lua'
   use 'rhysd/git-messenger.vim'
+  use {
+    'lewis6991/gitsigns.nvim',
+    -- tag = 'release' -- To use the latest release
+  }
   use 'psliwka/vim-smoothie'
   use 'lukas-reineke/indent-blankline.nvim'
-  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
 
   -- Utils
   use 'tpope/vim-fugitive'
@@ -44,10 +64,15 @@ require('packer').startup(function()
 
   -- Colorschemes
   use({ "catppuccin/nvim", as = "catppuccin" })
+  use 'norcalli/nvim-colorizer.lua'
+  use 'cormacrelf/dark-notify'
 
   -- Languages
   use 'sebdah/vim-delve'
   use 'ludovicchabant/vim-gutentags'
+
+  -- Misc
+  use 'LnL7/vim-nix'
 end)
 
 vim.o.expandtab = true
@@ -67,14 +92,16 @@ vim.o.smartcase = true
 vim.o.updatetime = 250
 vim.wo.signcolumn = 'yes'
 vim.o.completeopt = 'menuone,noselect'
-
--- vim.o.termguicolors = false
+vim.o.winbar = "%=%m %f"
+vim.o.laststatus = 3
+vim.o.splitbelow = true
+vim.o.cmdheight = 0
 
 -- latte, frappe, macchiato, mocha
-vim.g.catppuccin_flavour = "mocha" 
+vim.g.catppuccin_flavour = "latte"
+-- vim.g.catppuccin_flavour = "mocha"
 vim.cmd[[colorscheme catppuccin]]
 
-vim.o.splitbelow = true
 
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.g.mapleader = ' '
@@ -87,6 +114,113 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 vim.keymap.set({'t'}, '<Esc>', "<C-\\><C-n>", { silent = true })
+
+
+-- Telescope
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers)
+vim.keymap.set('n', '<leader>sb', require('telescope.builtin').current_buffer_fuzzy_find)
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags)
+vim.keymap.set('n', '<leader>st', require('telescope.builtin').tags)
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').grep_string)
+vim.keymap.set('n', '<leader>sp', require('telescope.builtin').live_grep)
+vim.keymap.set('n', '<leader>bb', function() require('telescope.builtin').live_grep({grep_open_files=true}) end)
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles)
+vim.keymap.set('n', '<leader>sf', function()
+  require('telescope.builtin').find_files { previewer = false }
+end)
+vim.keymap.set('n', '<leader>so', function()
+  require('telescope.builtin').tags { only_current_buffer = true }
+end)
+
+-- Diagnostics
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
+vim.keymap.set('n', '<leader>cd', vim.diagnostic.open_float)
+-- :lua vim.diagnostic.goto_next()
+-- vim.diagnostic.open_float(0, {scope="line"})
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true
+  }
+)
+
+local signs = {
+  { name = "DiagnosticSignError", text = "" },
+  { name = "DiagnosticSignWarn", text = "" },
+  { name = "DiagnosticSignHint", text = "" },
+  { name = "DiagnosticSignInfo", text = "" },
+}
+
+for _, sign in ipairs(signs) do
+  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+end
+
+
+-- LSP settings
+local lspconfig = require 'lspconfig'
+local on_attach = function(_, bufnr)
+  local opts = { buffer = bufnr }
+  -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'gi', require('telescope.builtin').lsp_implementations, opts)
+  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, opts)
+
+  -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+  -- vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+end
+
+-- nvim-cmp supports additional completion capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- Enable the following language servers
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'gopls', 'bashls' }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- Debugger
+vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<Leader>bp", ":DlvToggleBreakpoint<CR>")
+vim.keymap.set("n", "<F5>", ":DlvExec ./tilectl . --\\ server<CR>")
+vim.g.delve_new_command = 'new' -- open dlv in a horiztonal split
+vim.api.nvim_create_user_command("GoFmt", "!gofmt -w %", {})
+vim.api.nvim_create_user_command("GoBuild", "AsyncRun -mode=term -pos=bottom -rows=10 go build -gcflags='all=-N -l' ./cmd/tilectl", {})
+
+
+-- DAP
+-- vim.keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>")
+-- vim.keymap.set("n", "<F10>", ":lua require'dap'.step_over()<CR>")
+-- vim.keymap.set("n", "<F11>", ":lua require'dap'.step_into()<CR>")
+-- vim.keymap.set("n", "<F12>", ":lua require'dap'.step_out()<CR>")
+-- vim.keymap.set("n", "<Leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")
+-- vim.keymap.set("n", "<Leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+-- vim.keymap.set("n", "<Leader>lp", ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>")
+-- vim.keymap.set("n", "<Leader>dr", ":lua require'dap'.repl.open()<CR>")
+-- vim.keymap.set("n", "<Leader>dl", ":lua require'dap'.run_last()<CR>")
+-- vim.keymap.set("n", "<Leader>dl", ":lua require'dap'.run_last()<CR>")
+-- vim.keymap.set("n", "<Leader>td", ":lua require('dap-go').debug_test()<CR>")
+--
+-- require('dap-go').setup()
+-- require("dapui").setup()
+-- require('nvim-dap-virtual-text').setup()
+--
+-- local dap, dapui = require("dap"), require("dapui")
+-- dap.listeners.after.event_initialized["dapui_config"] = function()
+--   dapui.open()
+-- end
+-- dap.listeners.before.event_terminated["dapui_config"] = function()
+--   dapui.close()
+-- end
+-- dap.listeners.before.event_exited["dapui_config"] = function()
+--   dapui.close()
+-- end
 
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
@@ -105,19 +239,11 @@ require('indent_blankline').setup {
 
 }
 
--- Gitsigns
-require('gitsigns').setup {
-  signs = {
-    add = { hl = 'GitGutterAdd', text = '+' },
-    change = { hl = 'GitGutterChange', text = '~' },
-    delete = { hl = 'GitGutterDelete', text = '_' },
-    topdelete = { hl = 'GitGutterDelete', text = '‾' },
-    changedelete = { hl = 'GitGutterChange', text = '~' },
-  },
-}
+require('gitsigns').setup {}
 
 -- Telescope
 local action_layout = require("telescope.actions.layout")
+local actions = require('telescope.actions')
 require('telescope').setup {
   defaults = {
     layout_strategy = 'vertical',
@@ -130,29 +256,16 @@ require('telescope').setup {
         ['<C-u>'] = false,
         ['<C-d>'] = false,
         ['<C-space>'] = action_layout.toggle_preview,
+        ['<C-q>'] = actions.send_to_qflist,
       },
       n = {
         ['<C-space>'] = action_layout.toggle_preview,
+        ['<C-q>'] = actions.send_to_qflist,
       }
     },
   },
 }
 require('telescope').load_extension 'fzf'
-
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers)
-vim.keymap.set('n', '<leader>sf', function()
-  require('telescope.builtin').find_files { previewer = false }
-end)
-vim.keymap.set('n', '<leader>sb', require('telescope.builtin').current_buffer_fuzzy_find)
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags)
-vim.keymap.set('n', '<leader>st', require('telescope.builtin').tags)
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').grep_string)
-vim.keymap.set('n', '<leader>sp', require('telescope.builtin').live_grep)
-vim.keymap.set('n', '<leader>bb', function() require('telescope.builtin').live_grep({grep_open_files=true}) end)
-vim.keymap.set('n', '<leader>so', function()
-  require('telescope.builtin').tags { only_current_buffer = true }
-end)
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles)
 
 -- Treesitter configuration
 -- Parsers must be installed manually via :TSInstall
@@ -207,46 +320,6 @@ require('nvim-treesitter.configs').setup {
   },
 }
 
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
-
--- LSP settings
-local lspconfig = require 'lspconfig'
-local on_attach = function(_, bufnr)
-  local opts = { buffer = bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-  vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, opts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, opts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-  vim.keymap.set('n', '<leader>so', require('telescope.builtin').lsp_document_symbols, opts)
-  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-  vim.keymap.set('n', '<leader>wl', function()
-    vim.inspect(vim.lsp.buf.list_workspace_folders())
-  end, opts)
-  vim.api.nvim_create_user_command("Format", vim.lsp.buf.formatting, {})
-end
-
--- nvim-cmp supports additional completion capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
--- Enable the following language servers
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'gopls', 'bashls' }
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
-end
 
 -- luasnip setup
 local luasnip = require 'luasnip'
@@ -301,9 +374,6 @@ vim.api.nvim_command([[
 
 --vim.o.statusline = "%#Separator#%#Contents#%t%#Separator# %#Separator#%#Modified#%{&modified?'●':''}%#NotModified#%{&modified?'':'●'}%#Separator#%= %#Separator#%#Contents#%l,%c%#Separator# %#Separator#%#Contents#%p%%%#Separator# %#Separator#%#Contents#%Y%#Separator#"
 
-vim.o.winbar = "%=%m %f"
-vim.o.laststatus = 3
-
 require('lualine').setup {
   options = {
     -- theme = 'auto',
@@ -336,8 +406,5 @@ require('lualine').setup {
 }
 
 require'nvim-tree'.setup {}
-vim.keymap.set("n", "<Leader>bp", ":DlvToggleBreakpoint<CR>")
-vim.keymap.set("n", "<F5>", ":DlvExec ./tilectl . --\\ server<CR>")
-vim.g.delve_new_command = 'new' -- open dlv in a horiztonal split
-vim.api.nvim_create_user_command("GoFmt", "!gofmt -w %", {})
-vim.api.nvim_create_user_command("GoBuild", "AsyncRun -mode=term -pos=bottom -rows=10 go build -gcflags='all=-N -l' ./cmd/tilectl", {})
+
+require('colorizer').setup(nil, { css = true; })
