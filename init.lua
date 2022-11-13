18a0257ae7ba96cda52744a8171b67d1265b0ffb
Melanie Berkley <melanie@berkley.cio>
2022-11-13 02:00:29 -0800

    nvim 8 updates

--- init.lua
+++ init.lua
@@ -174,7 +174,7 @@ end
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Enable the following language servers
 local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'gopls', 'bashls' }
